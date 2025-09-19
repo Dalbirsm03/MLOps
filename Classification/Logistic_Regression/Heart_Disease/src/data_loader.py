79e26cd0cf60af754855dfadf31466a81631e4de
Dalbirsm03 <dalbirsm03@gmail.com>
2025-09-19 17:24:46 +0530

    commit

--- Classification/Logistic_Regression/Heart_Disease/src/data_loader.py
+++ Classification/Logistic_Regression/Heart_Disease/src/data_loader.py
@@ -5,6 +5,7 @@ def data_loader(path):
     df = pd.read_csv(path)
     X = df.drop(columns=["target"])
     Y = df['target']
+    
     X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.1, stratify=Y, random_state=1)
     
     return X_train, X_test, Y_train, Y_test
